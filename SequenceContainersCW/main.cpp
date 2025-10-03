86d391dac7a07aa507a55b6702a955b3785383dc
Danila <Danila@DESKTOP-44LP0PV>
2025-10-03 17:48:30 +0300

    List insert/erase, forward_list insert/erase: DONE

--- SequenceContainersCW/main.cpp
+++ SequenceContainersCW/main.cpp
@@ -1,6 +1,8 @@
 ﻿#include<iostream>
 #include<array>
 #include<vector>
+#include<forward_list>
+#include<list>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -12,14 +14,16 @@ template<typename T>void vector_info(const std::vector<T>& vec);
 
 //#define STL_ARRAY
 //#define FACTORIAL
-#define STL_VECTOR
+//#define STL_VECTOR
+//#define STL_LIST
+#define STL_FORWARD_LIST
+
 
 
 
 void main()
 {
 	setlocale(LC_ALL, "");
-
 #ifdef STL_ARRAY
 	//array - это контейнер, который хранит данные в виде статического массива;
 	throw;
@@ -119,7 +123,71 @@ void main()
 	vec.insert(vec.begin() + index, value);
 	for (int i : vec)cout << i << tab; cout << endl;
 #endif // STL_VECTOR
+
+#ifdef STL_LIST
+	std::list<int> numbers{ 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };	//Список
+	std::list<int>::iterator Iterator;							//Указатель на элемент
+	for (int n : numbers)
+		std::cout << n << "\t";									//Цикл для вывода списка
+	std::cout << std::endl;
+	Iterator = numbers.begin();									//Указываю на начальный элемент списка
+	for (int i = 0; i < 4; ++i)
+	{
+		++Iterator;												//Цикл для перемещения по списку
+	}
+	numbers.insert(Iterator, 10);
+	for (int n : numbers)
+		std::cout << n << "\t";									//Цикл для вывода списка
+	std::cout << std::endl;
+	for (int i = 0; i < 2; ++i)
+	{
+		++Iterator;												//Цикл для перемещения по списку
+	}
+	numbers.erase(Iterator);									//Удаляю элемент на который указывает итератор
+	for (int n : numbers)
+		std::cout << n << "\t";									//Цикл для вывода списка
+	std::cout << std::endl;
+#endif // STL_LIST
+
+#ifdef STL_FORWARD_LIST
+	std::forward_list<int> numbers = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };	//Односвязный список
+	std::forward_list<int>::iterator Iterator;								//указатель на элемент
+	for (int i : numbers)
+		std::cout << i << "\t";												//Цикл для вывода списка
+	std::cout << std::endl;
+	Iterator = numbers.before_begin();										//Указываю на начальный элемент списка
+	++Iterator;																//Перемещаю указатель на элемент
+	++Iterator;																//Перемещаю указатель на элемент
+	Iterator = numbers.insert_after(Iterator, 2, 30);						//Добавляю два раза по 30 на место указателя на элемент
+	for (int i : numbers)
+		std::cout << i << "\t";												//Вывожу односвязный список снова
+	std::cout << std::endl;
+	for (int i = 1; i < 5; ++i)
+	{
+		++Iterator;															//Перемещаю указатель на 5-ый элемент после последнего заданного указателя(он же является 9-ый по всему списку)
+	}
+	numbers.erase_after(Iterator);											//удаляю, элемент на который указывает указатель
+	for (int i : numbers)
+		std::cout << i << "\t";												//И опять вывожу список
+	std::cout << std::endl;
+	Iterator = numbers.before_begin();										//Ставлю указатель на нулевой элемент, чтобы прочесать весь односвязный список снова
+	for (int i = 0; i < 2; ++i)
+	{
+		++Iterator;															//Ставлю указатель на второй элемент(с нуля), или же третий с при счёте с единицы(это для себя, ибо я уже запутался во всем этом)				
+	}
+	for (int i = 0; i < 2; ++i)
+	{
+		numbers.erase_after(Iterator);										//Соответственно удаляю второй и третий элементы
+	}
+	//numbers.erase_after(Iterator);
+	for (int i : numbers)
+		std::cout << i << "\t";												//И опять всё вывожу на консоль
+	std::cout << std::endl;
+#endif // STL_FORWARD_LIST
+
 }
+
+
 
 long long int Factorial(int n)
 {
@@ -131,7 +199,6 @@ long long int Factorial(int n)
 	}
 	return f;
 }
-
 
 template<typename T>void vector_info(const std::vector<T>& vec)
 {
